2468356b17d854434b16cb8362a76b1b619e6bf5
Aarav Vaish <aaravvaish2004@gmail.com>
2026-02-22 00:31:51 +0530

    completed logic to find weather info

--- API_service/main.go
+++ API_service/main.go
@@ -3,30 +3,27 @@ package main
 import (
 	"fmt"
 
- geo "github.com/Aarav11-vaish/Rate-limiter_go_project/geo_location_through_IP"	
- "github.com/labstack/echo/v4"
+	fetch_weather_data "github.com/Aarav11-vaish/Rate-limiter_go_project/fetch_weather_data"
+	"github.com/labstack/echo/v4"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	e := echo.New()
+    if err := godotenv.Load("../.env"); err != nil {
+        fmt.Println("Warning: Could not load .env file:", err)
+    }
 
 	// print something
-	fmt.Println("Welcome to the server backend oF GO hit /getIP to get your IP address with region and country and / to get a welcome message")
+	fmt.Println("Welcome to the server backend oF GO hit /weather:{city name} to get the details and / to get a welcome message")
 
-	// wnat to use mvc pattern
-	e.GET("/getIP", func(c echo.Context) error {
-
-		// get the ip address of the client
-
-		ip := c.RealIP()
-		// get the region and country of the ip address
-
-		data, err := geo.Getlocation(ip)
+	e.GET("/weather/:city", func(c echo.Context) error {
+		city := c.Param("city")
+		weatherData, err := fetch_weather_data.FetchWeatherData(city)
 		if err != nil {
-			return c.String(500, "Error getting location")
+			return c.JSON(500, map[string]string{"error": "Failed to fetch weather data"})
 		}
-		return c.String(200, data)
-
+		return c.JSON(200, weatherData)
 	})
 
 	e.GET("/", func(c echo.Context) error {
